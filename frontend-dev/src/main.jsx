67a71eacfb088ce85051f63559cacc005042d17d
bhavyank89 <bhavyanksarolia@gmail.com>
2025-06-28 15:26:45 +0530

    removed frontend folder and added vercel analytics

--- frontend-dev/src/main.jsx
+++ frontend-dev/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Page'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <Router>
     <Home />
+    <Analytics />
   </Router>
 )
